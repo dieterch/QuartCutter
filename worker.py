a7b6c7acac37c2cf7a6c67cb2ee8953ce4c6a2fe
dieterch <dieter.chvatal@gmail.com>
2022-01-17 20:16:23 +0100

    worker: multiple trials to connect to redis

--- worker.py
+++ worker.py
@@ -1,6 +1,8 @@
 from redis import Redis
+from redis.exceptions import ConnectionError
 from rq import Connection, Queue, Worker
 from webcutter2.dcut import CutterInterface
+import time
 
 fileserver = '192.168.15.10'
 cutter = CutterInterface(fileserver)
@@ -8,6 +10,16 @@ cutter = CutterInterface(fileserver)
 redis_connection = Redis(host='localhost',port=6379,password='63nTa6UlGeRipER5HIlInTH5hoS3ckL4')
 
 if __name__ == '__main__':
-    with Connection(redis_connection):
-        worker = Worker(Queue('QuartCutter'))
-        worker.work()
+    for i in range(10):
+        try:
+            with Connection(redis_connection):
+                worker = Worker(Queue('QuartCutter'))
+                worker.work()
+                break
+        except (ConnectionError, ConnectionRefusedError) as err:
+        #except Exception as err:
+            print(str(err), f"... retry {i+1}/10 ...")
+            time.sleep(5)
+    print(f"giving up ... QuartCutter Worker Execution ended.")
+
+#redis.exceptions.ConnectionError
