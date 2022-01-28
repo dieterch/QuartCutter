e16fdda2e6c7e3513e61d2b5679b8a535e711ce2
dieterch <dieter.chvatal@gmail.com>
2022-01-28 22:46:51 +0100

    jinja2 inline xspf file

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-import io
+from io import BytesIO
 import subprocess
 import time
 import json
@@ -17,6 +17,26 @@ from rq.job import Job
 
 from webcutter2.dcut import CutterInterface #, report_success, report_failure
 from webcutter2.dplex import PlexInterface
+from jinja2 import Template
+
+xspf_template = """<?xml version="1.0" encoding="UTF-8"?>
+<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">
+	<title>Wiedergabeliste</title>
+	<trackList>{% for item in data %}
+		<track>
+            <title>{{ item['title'] }}</title>
+			<location>{{ item['url'] }}</location>
+			<duration>{{ item['dur'] }}</duration>
+			<extension application="http://www.videolan.org/vlc/playlist/0">
+				<vlc:id>{{ item['i'] }}</vlc:id>
+			</extension>
+		</track>{% endfor %}
+	</trackList>
+	<extension application="http://www.videolan.org/vlc/playlist/0">
+		<vlc:item tid="0"/>
+	</extension>
+</playlist>"""
+
 
 fileserver = '192.168.15.10'
 baseurl = 'http://192.168.15.10:32400'
@@ -272,37 +292,33 @@ async def analyse():
         else:
             return 'not ok' 
 
+@app.route('/streamsection.xspf')
+async def streamsection():
+    global selection
+    movies = selection['movies']
+    data = []
+    for i,m in enumerate(movies):
+        data.append({'i':i, 'title':m.title, 'url': m.getStreamURL().replace('&','&amp;'), 'dur': m.duration})
+    j2_template = Template(xspf_template)
+    w = j2_template.render(data=data)    
+    b = BytesIO(w.encode('utf-8','xmlcharrefreplace'))
+    b.seek(0)
+    return await send_file(b, as_attachment=True,
+         attachment_filename='streamsection.xspf',
+         mimetype='text/xspf+xml')
+
 @app.route('/streamurl.xspf')
 async def streamurl():
-    """
-    create an *.xspf file for vlc on the fly.
-    """
     global selection
     m = selection['movie']
-    #url = 'http://192.168.15.10:32400/video/:/transcode/universal/start.m3u8?X-Plex-Platform=Chrome&amp;copyts=1&amp;mediaIndex=0&amp;offset=0&amp;path=%2Flibrary%2Fmetadata%2F20317&amp;X-Plex-Token=7YgcyPLqGVM-PVxq2QVo'
-    url = m.getStreamURL().replace('&','&amp;')
-    #duration = 1424560
-    duration = m.duration
-    tfile =f"""<?xml version="1.0" encoding="UTF-8"?>
-<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">
-	<title>Wiedergabeliste</title>
-	<trackList>
-		<track>
-			<location>{url}</location>
-			<duration>{duration}</duration>
-			<extension application="http://www.videolan.org/vlc/playlist/0">
-				<vlc:id>0</vlc:id>
-			</extension>
-		</track>
-	</trackList>
-	<extension application="http://www.videolan.org/vlc/playlist/0">
-		<vlc:item tid="0"/>
-	</extension>
-</playlist>
-"""
-    with open('static/stream.xspf', 'w', encoding='utf-8') as f:
-        f.write(tfile)
-    return await send_file('static/stream.xspf')
+    data = [{'i':0, 'title':m.title, 'url': m.getStreamURL().replace('&','&amp;'), 'dur': m.duration}]
+    j2_template = Template(xspf_template)
+    w = j2_template.render(data=data)    
+    b = BytesIO(w.encode('utf-8','xmlcharrefreplace'))
+    b.seek(0)
+    return await send_file(b, as_attachment=True,
+         attachment_filename='streamurl.xspf',
+         mimetype='text/xspf+xml')
 
 @app.route("/timeline", methods=['POST'])
 async def timeline():
